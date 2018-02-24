032004d8729fd1fdc0a2de5f95ac69aaf8974531
BlasTJSN <blast1030968239@gmail.com>
2018-02-24 16:24:10 +0800

    注册页面使用类视图

--- dailyfresh/apps/users/urls.py
+++ dailyfresh/apps/users/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from users.views import *
 
 urlpatterns = [
-    url(r"^register$", register, name="register"),
+    url(r"^register$", RegisterView.as_view(), name="register"),
 ]
--- dailyfresh/apps/users/views.py
+++ dailyfresh/apps/users/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,HttpRequest
+from django.views.generic import View
 # Create your views here.
 
+# 类视图的使用
+class RegisterView(View):
+    """类视图：处理注册"""
 
-def register(request):
-    """返回注册页面"""
-    return render(request,"register.html")
+    def get(self,requset):
+        """处理GET请求，返回注册页面"""
+        return render(requset,"register.html")
+
+    def post(self,request):
+        """处理POST请求，实现注册逻辑"""
+        return HttpResponse("这里实现注册逻辑")
