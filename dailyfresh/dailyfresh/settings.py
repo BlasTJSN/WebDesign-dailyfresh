1ca479663b104428c6e79a793b8d5b024908eb18
JIN <blast1030968239@gmail.com>
2019-04-30 11:19:29 +0800

    Update settings.py

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -135,9 +135,9 @@ AUTH_USER_MODEL = "users.User"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # 导入邮件模块
 EMAIL_HOST = 'smtp.qq.com' # 发邮件主机
 EMAIL_PORT = 587 # 发邮件端口
-EMAIL_HOST_USER = '3393133521@qq.com' # 授权的邮箱
-EMAIL_HOST_PASSWORD = 'ctsvjxbnixsadaaj' # 邮箱授权时获得的密码，非注册登录密码
-EMAIL_FROM = '天天生鲜<3393133521@qq.com>' # 发件人抬头
+EMAIL_HOST_USER = 'xxxxxxx@qq.com' # 授权的邮箱
+EMAIL_HOST_PASSWORD = 'xxxxx' # 邮箱授权时获得的密码，非注册登录密码
+EMAIL_FROM = '天天生鲜<xxxxxxx@qq.com>' # 发件人抬头
 
 
 # 缓存
